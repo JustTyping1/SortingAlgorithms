a1e698e272342f950e30b5792f4efce80da79637
JustTyping1 <aditya.chorghade@outlook.com>
2022-09-17 11:12:17 +0100

    basic switching list position logic

--- main.cs
+++ main.cs
@@ -8,13 +8,22 @@ class sortingalgorithms{
 
   public static void bubblesort () {
     List<int> values = new List<int>();
-    values.Add(3);
     values.Add(5);
+    values.Add(3);
     values.Add(2);
     values.Add(4);
     values.Add(1);
-    foreach(var value in values) {
-      Console.WriteLine(value);
+
+    if(values[0] >= values[1]){
+      int val1 = values[0];
+      int val2 = values[1];
+
+      values[0] = val2;
+      values[1] = val1;
+
+      foreach(var value in values){
+        Console.WriteLine(value);
+      }
     }
 
   }
